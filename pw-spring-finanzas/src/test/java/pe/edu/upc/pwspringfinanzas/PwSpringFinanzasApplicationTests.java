9542abd49e18c631287a4733cda4ca2aa182a729
John <John@DESKTOP-HDVOJCL>
2021-06-18 14:18:25 -0500

    Creación del Proyecto

---
+++ pw-spring-finanzas/src/test/java/pe/edu/upc/pwspringfinanzas/PwSpringFinanzasApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.pwspringfinanzas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwSpringFinanzasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
